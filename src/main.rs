3e3b44f08af9b7139e4b1129f65066fa48709275
Nikolay Neupokoev <neupokoev.n@gmail.com>
2022-07-27 19:56:17 -0700

    Generate PNG

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use cairo::{Format,
     cairo_show_text, 
     cairo_image_surface_create, 
     cairo_create, 
-    cairo_font_extents, 
+    cairo_font_extents,
+    cairo_select_font_face, 
+    cairo_set_font_size,
     cairo_text_extents,
-    STATUS_SUCCESS,
-    cairo_status_t,
+    FONT_SLANT_NORMAL,
+    FONT_WEIGHT_BOLD,
   }, ImageSurface
   
 };
@@ -50,47 +52,53 @@ impl Empty<TextExtents> for TextExtents {
   }
 }
 
-
 fn main() {
-
   // output size
-  let width = 120;
-  let height = 120;
+  let width = 1024;
+  let height = 768;
   
-  unsafe{
-    let surface = cairo_image_surface_create(
-      i32::from(Format::ARgb32), width, height);
-    let context = cairo_create(surface);
 
-    // https://gtk-rs.org/gtk-rs-core/stable/latest/docs/cairo/struct.FontExtents.html
-    let mut fe: FontExtents = FontExtents::empty();
-    cairo_font_extents(context, &mut fe);
+  let surface = unsafe {cairo_image_surface_create(
+    i32::from(Format::ARgb32), width, height)};
+  let context = unsafe {cairo_create(surface)};
 
-    let alphabet = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+  let font_family = b"Source Code Pro\0".as_ptr();
+  unsafe {cairo_select_font_face(context, font_family as *const i8,
+    FONT_SLANT_NORMAL,
+    FONT_WEIGHT_BOLD)};
+
+  unsafe {cairo_set_font_size(context, 60.0)};
+
+  // https://gtk-rs.org/gtk-rs-core/stable/latest/docs/cairo/struct.FontExtents.html
+  let mut fe: FontExtents = FontExtents::empty();
+  unsafe {cairo_font_extents(context, &mut fe)};
+
+  let alphabet = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+  
+  let mut te: TextExtents = TextExtents::empty();
+  for (i, letter) in alphabet.chars().enumerate() {
+    let text: [i8; 2] = [letter as i8, '\0' as i8];
     
-    let mut te: TextExtents = TextExtents::empty();
-    for (i, letter) in alphabet.chars().enumerate() {
-      let text: [i8; 2] = [letter as i8, '\0' as i8];
-      
-      cairo_text_extents(context, &text as *const i8, &mut te);
-      
-      let x = (i as f64) + 0.5 - te.x_bearing - te.width / 2.0;
-      let y = 0.5 - fe.descent + fe.height / 2.0;
-      cairo_move_to(context, x, y);
+    unsafe {cairo_text_extents(context, &text as *const i8, &mut te)};
     
-      cairo_show_text(context, &text as *const i8);
-    }
+    let x = (i as f64) * 40.0 + 25.0 - te.x_bearing - te.width / 2.0;
+    let y = 60.0 - fe.descent + fe.height / 2.0;
+    unsafe {cairo_move_to(context, x, y)};
+  
+    unsafe {cairo_show_text(context, &text as *const i8)};
+  }
 
-    let result = ImageSurface::from_raw_full(surface);
-    if result.is_ok() {
-      let image = result.expect("image surface wow");
-      let mut file_result = File::create("text.png");
-      if file_result.is_ok() {
-        let file = file_result.expect("file is open");
-        image.write_to_png(file);
+  let result = unsafe {ImageSurface::from_raw_full(surface)};
+  if result.is_ok() {
+    let image = result.expect("image surface wow");
+    let file_result = File::create("text.png");
+    if file_result.is_ok() {
+      let mut file = file_result.expect("file is open");
+      let write_result = image.write_to_png(&mut file);
+      if write_result.is_err() {
+        write_result.err();
       }
     }
-
   }
 
 }
