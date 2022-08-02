b78c1069833334e15973bd0f0a892e0e0c1f046c
Nikolay Neupokoev <neupokoev.n@gmail.com>
2022-08-01 20:12:18 -0700

    Combine all together

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use chrono::{Datelike, NaiveDate, NaiveDateTime, Duration};
 use cairo::{Format,
   FontExtents, 
   TextExtents, 
+  Context,
   Surface,
   ffi::{cairo_move_to, 
     cairo_show_text, 
@@ -12,6 +13,7 @@ use cairo::{Format,
     cairo_select_font_face, 
     cairo_set_font_size,
     cairo_text_extents,
+    cairo_surface_get_reference_count,
     FONT_SLANT_NORMAL,
     FONT_WEIGHT_BOLD, cairo_surface_t,
   },
@@ -24,23 +26,58 @@ use cairo::{Format,
 // create commits by specifing dates https://stackoverflow.com/questions/454734/how-can-one-change-the-timestamp-of-an-old-commit-in-git
 
 fn text_to_dots(text: String) -> Vec<[u8; 7]> {
-    // load font
-
     // use font to render a text -> picture
-    let mut image = text_to_surface(text);
-    let data = image.data().unwrap_or_else(|error| {
+    let mut surface = text_to_surface(text);
+    let width: usize = surface.width() as usize;
+    let height: usize = surface.height() as usize;
+    save_surface_as_png(&surface);
+    // println!("reference counter z {}", unsafe {cairo_surface_get_reference_count(surface.to_raw_none())});
+    let data = surface.data().unwrap_or_else(|error| {
       panic!("no data in the surface: {}", error.to_string());
     });
-    println!("image surface length {}", data.len());
+    // println!("image data length {}", data.len());
+    // for y in 0..height {
+    //     for x in 0..width {
+    //       // print!("{}", x * 4 + y * width * 4);
+    //       let r = data[x * 4 + y * width * 4];
+    //       let g = data[x * 4 + y * width * 4 + 1];
+    //       let b = data[x * 4 + y * width * 4 + 2];
+    //       let a = data[x * 4 + y * width * 4 + 3];
+    //       print!("{}", if r > 0 || g > 0 || b > 0 || a > 0 {"X"} else {"_"});
+    //   }
+    //   println!("");
+    // }
 
     // on a matrix highlight cells (dots) that correspond to the outlines
     // in other words: picture to matrix
-
-    
     let mut dots = Vec::new();
-    let mut week: [u8; 7] = [0; 7];
 
-    dots.push(week);
+    let threshold = 30.0;
+    let box_size = height / 7;
+    println!("box size {box_size}");
+    for i_x in 0..(width / box_size) {
+      let mut week: [u8; 7] = [0; 7];
+      for i_y in 0..(height / box_size) {
+        let mut total_box_color: u64 = 0;
+        for y in i_y * box_size..(i_y + 1) * box_size{
+          for x in i_x * box_size..(i_x + 1) * box_size {
+            // print!("{}", x * 4 + y * width * 4);
+            let r = data[x * 4 + y * width * 4];
+            let g = data[x * 4 + y * width * 4 + 1];
+            let b = data[x * 4 + y * width * 4 + 2];
+            let a = data[x * 4 + y * width * 4 + 3];
+            total_box_color += (r + g + b + a) as u64
+          }
+        }
+        // print!(" {} ", if total_box_color as f64 / (4 * box_size * box_size) as f64 > 1.0 {"X"} else {"_"});
+        // print!("{}", if total_box_color as f64 / (4 * box_size * box_size) as f64 > threshold {"X"} else {"_"});
+        let busy_day = if total_box_color as f64 / (4 * box_size * box_size) as f64 > threshold {1} else {0};
+        week[i_y] = busy_day;
+      }
+      // println!("");
+      dots.push(week);
+    }
+
     dots
 }
 
@@ -65,7 +102,7 @@ fn dots_to_dates(start_date: NaiveDate, dots: Vec<[u8; 7]>) -> Vec<[Option<Naive
     dates
 }
 
-fn print_dots(dots: Vec<[u8; 7]>) {
+fn print_dots(dots: &Vec<[u8; 7]>) {
     for week_day in 0..7 {
         let mut column = 0;
         while column < dots.len() {
@@ -134,52 +171,50 @@ impl Empty<TextExtents> for TextExtents {
 
 fn text_to_png(text: String) {
   let surface = text_to_surface(text);
-  save_surface_as_png(surface);
+  save_surface_as_png(&surface);
 }
 
 fn text_to_surface(text: String) -> ImageSurface {
   // output size
-  let width = 1024;
-  let height = 768;
+  let width = 250;
+  let height = 60;
   
   let surface_ptr = unsafe {cairo_image_surface_create(
     i32::from(Format::ARgb32), width, height)};
-  let surface: ImageSurface = unsafe {ImageSurface::from_raw_borrow(surface_ptr)};
-
-  // let surface = if surface_ptr.is_null() {
-  //   None
-  // } else {
-  //   Some(surface_ptr as *mut cairo_surface_t)
-  // };
-  let context = unsafe {cairo_create(surface_ptr)};
+  // println!("reference counter a {}", unsafe {cairo_surface_get_reference_count(surface_ptr)});
+  let surface: ImageSurface = unsafe {ImageSurface::from_raw_full(surface_ptr).unwrap()};
+  // println!("reference counter b {}", unsafe {cairo_surface_get_reference_count(surface_ptr)});
+  let context: Context = Context::new(&surface).unwrap();
+  let context_ptr = context.to_raw_none();
+  // println!("reference counter c {}", unsafe {cairo_surface_get_reference_count(surface_ptr)});
 
   let font_family = b"Source Code Pro\0".as_ptr();
-  unsafe {cairo_select_font_face(context, font_family as *const i8,
+  unsafe {cairo_select_font_face(context_ptr, font_family as *const i8,
     FONT_SLANT_NORMAL,
     FONT_WEIGHT_BOLD)};
 
-  unsafe {cairo_set_font_size(context, 60.0)};
+  unsafe {cairo_set_font_size(context_ptr, 70.0)};
 
   // https://gtk-rs.org/gtk-rs-core/stable/latest/docs/cairo/struct.FontExtents.html
   let mut fe: FontExtents = FontExtents::empty();
-  unsafe {cairo_font_extents(context, &mut fe)};
+  unsafe {cairo_font_extents(context_ptr, &mut fe)};
 
   let mut te: TextExtents = TextExtents::empty();
   let chars = text.as_bytes().as_ptr();
-  unsafe {cairo_text_extents(context, chars as *const i8, &mut te)};
+  unsafe {cairo_text_extents(context_ptr, chars as *const i8, &mut te)};
   
   let x = te.x_bearing;
-  let y = fe.height - fe.descent;
-  // println!("{}  {} {} {}", te.x_bearing, te.width, fe.descent, fe.height);
+  let y = fe.height - fe.descent - 15.0;
+  println!("{}  {} {} {}", te.x_bearing, te.width, fe.descent, fe.height);
   
-  unsafe {cairo_move_to(context, x, y)};
+  unsafe {cairo_move_to(context_ptr, x, y)};
 
-  unsafe {cairo_show_text(context, chars as *const i8)};
+  unsafe {cairo_show_text(context_ptr, chars as *const i8)};
 
   surface
 }
 
-fn save_surface_as_png(surface: ImageSurface) {
+fn save_surface_as_png(surface: &ImageSurface) {
   let mut file = File::create("text.png").unwrap();
   surface.write_to_png(&mut file).unwrap();
 }
@@ -237,18 +272,11 @@ fn test_cairo() {
 fn main() {
   let text: String = "Hello".to_string();
   let dots = text_to_dots(text);
-  // let dots = vec![
-  //     [0,1,1,1,1,1,0],
-  //     [0,1,1,0,1,1,0],
-  //     [1,1,1,1,1,1,1]
-  // ];
-  //print_dots(dots);
+  print_dots(&dots);
 
-  // let start_date = NaiveDate::from_ymd(2022, 12, 29);
-  // println!("start date: {}", start_date.format("%Y-%m-%d").to_string());
-  // let dates = dots_to_dates(start_date, dots);
-  // print_dates(dates);
+  let start_date = NaiveDate::from_ymd(2022, 02, 27);
+  println!("start date: {}", start_date.format("%Y-%m-%d").to_string());
+  let dates = dots_to_dates(start_date, dots);
+  print_dates(dates);
 
-  // text_to_png(text)
-  // text_to_dots(text);
 }
